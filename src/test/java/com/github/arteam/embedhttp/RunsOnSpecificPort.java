5192328baeb0dd1c29643963569d955fe161e052
Artem Prigoda <arteamon@gmail.com>
2016-11-28 15:14:13 +0100

    Verify the server can be run on a specific port

---
+++ src/test/java/com/github/arteam/embedhttp/RunsOnSpecificPort.java
@@ -0,0 +1,44 @@
+package com.github.arteam.embedhttp;
+
+import com.github.arteam.embedhttp.junit.EmbeddedHttpServerRule;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Artem Prigoda
+ * @since 28.11.16
+ */
+public class RunsOnSpecificPort {
+
+    @Rule
+    public EmbeddedHttpServerRule httpServerRule = new EmbeddedHttpServerRule()
+            .withPort(15678)
+            .addHandler("/hello", (request, response) -> response.setBody("Hello, World!")
+                    .addHeader("content-type", "text/plain"));
+
+    private CloseableHttpClient httpClient = HttpClients.createMinimal();
+
+    @After
+    public void tearDown() throws Exception {
+        httpClient.close();
+    }
+
+    @Test
+    public void test() throws Exception {
+        HttpGet httpGet = new HttpGet("http://127.0.0.1:15678/hello");
+        String response = httpClient.execute(httpGet, httpResponse -> {
+            assertThat(httpResponse.getStatusLine().getStatusCode(), CoreMatchers.equalTo(200));
+            assertThat(httpResponse.getFirstHeader("Content-Type").getValue(), CoreMatchers.equalTo("text/plain"));
+            return EntityUtils.toString(httpResponse.getEntity());
+        });
+        assertThat(response, CoreMatchers.equalTo("Hello, World!"));
+    }
+}
