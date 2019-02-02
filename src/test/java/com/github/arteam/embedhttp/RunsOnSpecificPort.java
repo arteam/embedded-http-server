bd100f1e2f16070509b9d0ffb9a578d36aec59ee
Artem Prigoda <artem.prigoda@goeuro.com>
2019-02-02 13:24:49 +0100

    Move to JUnit5

--- src/test/java/com/github/arteam/embedhttp/RunsOnSpecificPort.java
+++ src/test/java/com/github/arteam/embedhttp/RunsOnSpecificPort.java
@@ -1,13 +1,13 @@
 package com.github.arteam.embedhttp;
 
-import com.github.arteam.embedhttp.junit.EmbeddedHttpServerRule;
+import com.github.arteam.embedhttp.junit.EmbeddedHttpServerExtension;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.junit.After;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,21 +18,21 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class RunsOnSpecificPort {
 
-    @Rule
-    public EmbeddedHttpServerRule httpServerRule = new EmbeddedHttpServerRule()
+    @RegisterExtension
+    public EmbeddedHttpServerExtension httpServerExtension = new EmbeddedHttpServerExtension()
             .withPort(15678)
             .addHandler("/hello", (request, response) -> response.setBody("Hello, World!")
                     .addHeader("content-type", "text/plain"));
 
     private CloseableHttpClient httpClient = HttpClients.createMinimal();
 
-    @After
-    public void tearDown() throws Exception {
+    @AfterEach
+    void tearDown() throws Exception {
         httpClient.close();
     }
 
     @Test
-    public void test() throws Exception {
+    void test() throws Exception {
         HttpGet httpGet = new HttpGet("http://127.0.0.1:15678/hello");
         String response = httpClient.execute(httpGet, httpResponse -> {
             assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
